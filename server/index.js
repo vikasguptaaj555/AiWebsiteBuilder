26328dbd4c465e33346ee5832d5eb68631d32ef2
Vikas Gupta <vikasguptaaj555@gmail.com>
2026-09-21 21:55:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://aiwebsitebuilder-1-6f99.onrender.com",
     credentials:true
 }))
 app.use("/api/auth",authRouter)
