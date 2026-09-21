4c918c1349fa129f136ede738b4b15a895e77c9e
Vikas Gupta <vikasguptaaj555@gmail.com>
2026-09-21 21:21:50 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import WebsiteEditor from './pages/Editor'
 import LiveSite from './pages/LiveSite'
 import Pricing from './pages/Pricing'
 
-export const serverUrl = import.meta.env.VITE_API_URL;
+export const serverUrl = "https://aiwebsitebuilder-9t5s.onrender.com";
 
 function App() {
   useGetCurrentUser()
